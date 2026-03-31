ca1c102e37cf1244767da2f0799c482783861f96
Abhishek Raj <abhishekraj0310@gmail.com>
2026-04-01 00:03:30 +0530

    added search insert position solutiom

---
+++ search_insert_position.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l=0;
+        int r=nums.size()-1;
+        int mid;
+        while(l<=r)
+        {
+            mid=l+(r-l)/2;
+            if(target==nums[mid])
+            return mid;
+            else if(target<nums[mid])
+            {
+                r=mid-1;
+            }
+            else if(target>nums[mid])
+            {
+                l=mid+1;
+            }
+        }
+        return l;
+        
+    }
+};
